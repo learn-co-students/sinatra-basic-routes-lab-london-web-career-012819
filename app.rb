f8099efed3b7cc90b86317253ed4fc24b4d0840b
SofiaFredriksson <sofiapkf@gmail.com>
2019-02-15 13:49:14 +0000

    Done

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+  "Hello, World!"
+  end
+
+  get '/name' do
+  "My name is Sofia"
+  end
+
+  get '/hometown' do
+  "My hometown is Lausanne"
+  end
+
+  get '/favorite-song' do
+  "My favorite song is 21 Questions by Winston Surfshirt (for now...)"
+  end
+
 end
