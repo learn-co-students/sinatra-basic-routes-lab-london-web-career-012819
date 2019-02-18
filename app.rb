55041d1ea9bbfe061cd57c664725f47b1b7d3680
Alexander Dubois <sander.dubois@outlook.com>
2019-02-18 13:50:45 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Sander"
+  end
+  get '/hometown' do
+    "My hometown is Göteborg"
+  end
+  get '/favorite-song' do
+    "My favorite song is Liquid Spirit"
+  end
+
 end
